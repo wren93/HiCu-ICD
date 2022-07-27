fad8c149a85a0d865869fb20e8ae65e14eced88a
wren93 <wren@cs.toronto.edu>
2022-07-26 22:28:36 -0400

    add runs

---
+++ runs/run_multirescnn_hicuc.sh
@@ -0,0 +1,19 @@
+python main.py 
+    --MODEL_DIR /path/to/project/root/models \
+    --DATA_DIR /path/to/project/root/data \
+    --MIMIC_3_DIR /path/to/project/root/data/mimic3 \
+    --data_path /path/to/project/root/data/mimic3/train_full.csv \
+    --embed_file /path/to/project/root/data/mimic3/processed_full_100.embed \
+    --vocab /path/to/project/root/data/mimic3/vocab.csv \
+    --Y full \
+    --model MultiResCNN \
+    --decoder HierarchicalHyperbolic \
+    --criterion prec_at_8 \
+    --MAX_LENGTH 4096 \
+    --batch_size 8  \
+    --lr 5e-5 \
+    --depth 5 \
+    --n_epochs '2,3,5,10,500'  \
+    --num_workers 8 \
+    --hyperbolic_dim 50 \
+    --cat_hyperbolic
---
+++ runs/run_rac_hicuc.sh
@@ -0,0 +1,21 @@
+python main.py \
+    --MODEL_DIR /path/to/project/root/models \
+    --DATA_DIR /path/to/project/root/data \
+    --MIMIC_3_DIR /path/to/project/root/data/mimic3 \
+    --data_path /path/to/project/root/data/mimic3/train_full.csv \
+    --embed_file /path/to/project/root/data/mimic3/processed_full_300.embed \
+    --vocab /path/to/project/root/data/mimic3/vocab_rac.csv \
+    --Y full \
+    --model RACReader \
+    --batch_size 16 \
+    --lr 8e-5 \
+    --criterion prec_at_8 \
+    --gpu '0,1,2,3' \
+    --tune_wordemb \
+    --MAX_LENGTH 4096 \
+    --num_workers 8 \
+    --filter_size 9 \
+    --n_epochs '2,3,5,7,500' \
+    --decoder HierarchicalHyperbolic \
+    --dropout 0.1 \
+    --cat_hyperbolic
